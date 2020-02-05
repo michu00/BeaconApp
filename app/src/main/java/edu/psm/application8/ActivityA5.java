4c7598da9b5c30821897b7e980e5e62c0be17ea4
mickey <michall.jackowski@gmail.com>
2020-02-05 11:19:57 +0100

    first version of the app

---
+++ app/src/main/java/edu/psm/application8/ActivityA5.java
@@ -0,0 +1,14 @@
+package edu.psm.application8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityA5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a2);
+    }
+}
---
+++ app/src/main/java/edu/psm/application8/ShowExercise.java
@@ -0,0 +1,14 @@
+package edu.psm.application8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_exercise);
+    }
+}
